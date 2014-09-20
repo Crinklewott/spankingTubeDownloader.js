09a8c14ee980b609558541ef212172c440154f57
thingywhat <thingywhat@live.com>
2014-09-20 04:35:31 -0600

    Add additional metadata for GreaseMonkey.

--- spankingTubeDownloader.js
+++ spankingTubeDownloader.js
@@ -1,8 +1,11 @@
 // ==UserScript==
 // @name          SpankingTube video downloader
+// @namespace     SpankingTube video downloader
 // @description	  Adds a download button to download videos off of SpankingTube
 // @author        thingywhat
 // @include       http://www.spankingtube.com/video/*
+// @version       1
+// @grant         none
 // ==/UserScript==
 
 $('.video_description').append($('<a />',{style:"cursor:pointer"}).click(function(){
