8caf38c3d495fad05eccb74d5c9895fb46ac29d4
thingywhat <thingywhat@live.com>
2023-05-29 20:58:01 -0600

    Unbroke this long-dead userscript again~

--- spankingTubeDownloader.user.js
+++ spankingTubeDownloader.user.js
@@ -3,15 +3,18 @@
 // @namespace     SpankingTube video downloader
 // @description	  Adds a download button to download videos off of SpankingTube
 // @author        thingywhat
-// @include       http://www.spankingtube.com/video/*
-// @version       1
+// @match         https://www.spankingtube.com/video/*
+// @version       2
 // @grant         none
 // ==/UserScript==
 
-$('#embed_video > a').unbind('click').removeAttr('href').click(function(){
-  var vkey = "//www.spankingtube.com/media/player/config_embed.php?vkey=";
-  $.get(vkey + /\/(\d+)\//.exec(location.href)[1], function(dat){
-    location.replace(dat.querySelector("hd").textContent ||
-		     dat.querySelector("src").textContent);
-  });
-}).find('span').text("Download");
+(function() {
+    'use strict';
+
+    let downloadButton = document.querySelector("#video_share > span");
+    downloadButton.textContent = " Download!";
+    downloadButton.onclick = function(e){
+        e.stopPropagation();
+        location.href = document.querySelector('#video source').src;
+    };
+})();
